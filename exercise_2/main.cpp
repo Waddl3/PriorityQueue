c698b5f7bfb9897aa467f7da9f30e7e97b216586
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-10-12 08:38:37 -0700

    Exercise 4 complete, ALL exercise have been completed

--- exercise_2/main.cpp
+++ exercise_2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
 #include "ListPriorityQueue.h"
 
 int main(int argc, char const *argv[])
@@ -7,51 +8,89 @@ int main(int argc, char const *argv[])
     ListPriorityQueue<int, Comparator<int>> pq;
     ListPriorityQueue<int, Comparator<int>> pq2;
     int num = 0;
-    int count = 5;
+    clock_t start;
+    clock_t end;
 
-    //small1k.txt
     std::fstream small_inFile("C:/Users/jesus/GitHub/PriorityQueue/small1k.txt", std::ios_base::in);
-
-    //large100k.txt
     std::fstream large_inFile("C:/Users/jesus/GitHub/PriorityQueue/large100k.txt", std::ios::in);
 
+    std::cout << "Author: Jesus Rodriguez-Luna" << std::endl << std::endl;
 
-    //check if file is not open
-    if(!small_inFile.is_open()) {
+    if (!small_inFile.is_open()) {
         std::cout << "Error: Unable to open file." << std::endl;
         return 1;
     }
-    
-    while(small_inFile >> num){
+
+    start = clock();
+    while (small_inFile >> num) {
         pq.insert(num);
     }
 
     small_inFile.close();
     std::cout << "small1k.txt File: ";
-    while(!pq.empty() && count--) {
+    int firstFiveCount = 5;
+
+    while (!pq.empty() && firstFiveCount--) {
         std::cout << pq.min() << " ";
         pq.removeMin();
     }
 
-    std::cout << std::endl;
-    count = 5;
+    firstFiveCount = 5;
 
-    //check if file is not open
-    if(!large_inFile.is_open()) {
+    std::cout << "\nlast 5: ";
+    std::list<int> lastFive;
+    while (!pq.empty()) {
+        lastFive.push_back(pq.min());
+        if (lastFive.size() > 5) {
+            lastFive.pop_front();
+        }
+        pq.removeMin();
+    }
+
+    for (const int& val : lastFive) {
+        std::cout << val << " ";
+    }
+
+    end = clock();
+    std::cout << "\nTime taken for small1k.txt sorting: " << end - start << " ms" << std::endl << std::endl;
+
+    if (!large_inFile.is_open()) {
         std::cout << "Error: Unable to open file." << std::endl;
         return 1;
     }
 
-    while(large_inFile >> num) {
+    start = clock();
+    while (large_inFile >> num) {
         pq2.insert(num);
     }
 
     large_inFile.close();
     std::cout << "large100k.txt File: ";
-    while(!pq2.empty() && count--) {
-        std::cout << pq.min() << " ";
+    firstFiveCount = 5;
+
+    while (!pq2.empty() && firstFiveCount--) {
+        std::cout << pq2.min() << " ";
         pq2.removeMin();
     }
+
+    firstFiveCount = 5;
+
+    std::cout << "\nlast 5: ";
+    std::list<int> lastFive2;
+    while (!pq2.empty()) {
+        lastFive2.push_back(pq2.min());
+        if (lastFive2.size() > 5) {
+            lastFive2.pop_front();
+        }
+        pq2.removeMin();
+    }
+
+    for (const int& val : lastFive2) {
+        std::cout << val << " ";
+    }
+
+    end = clock();
+    std::cout << "\nTime taken for large100k.txt sorting: " << end - start << " ms" << std::endl;
 
     return 0;
 }
--- exercise_4/main.cpp
+++ exercise_4/main.cpp
@@ -1,42 +1,101 @@
 #include <iostream>
+#include <fstream>
+#include <ctime>
 #include "HeapPriorityQueue.h"
+#include <list>
 
 int main(int argc, char const *argv[])
 {
     HeapPriorityQueue<int, Comparator<int>> pq;
+    HeapPriorityQueue<int, Comparator<int>> pq2;
+    int num = 0;
+    clock_t start;
+    clock_t end;
 
-    pq.insert(5);
-    pq.insert(4);
-    pq.insert(7);
-    pq.insert(1);
+    std::fstream small_inFile("C:/Users/jesus/GitHub/PriorityQueue/small1k.txt", std::ios_base::in);
+    std::fstream large_inFile("C:/Users/jesus/GitHub/PriorityQueue/large100k.txt", std::ios::in);
 
-    std::cout << pq.min() << " ";
+    std::cout << "Author: Jesus Rodriguez-Luna" << std::endl << std::endl;
 
-    pq.removeMin();
-    pq.insert(3);
-    pq.insert(6);
-    
-    std::cout << pq.min() << " ";
+    if (!small_inFile.is_open()) {
+        std::cout << "Error: Unable to open file." << std::endl;
+        return 1;
+    }
 
-    pq.removeMin();
+    start = clock();
+    while (small_inFile >> num) {
+        pq.insert(num);
+    }
 
-    std::cout << pq.min() << " ";
+    small_inFile.close();
+    std::cout << "small1k.txt File: ";
+    int firstFiveCount = 5;
 
-    pq.removeMin();
-    pq.insert(8);
+    while (!pq.empty() && firstFiveCount--) {
+        std::cout << pq.min() << " ";
+        pq.removeMin();
+    }
 
-    std::cout << pq.min() << " ";
+    firstFiveCount = 5;
 
-    pq.removeMin();
-    pq.insert(2);
+    std::cout << "\nlast 5: ";
+    std::list<int> lastFive; // Use a list to store the last 5 elements
+    while (!pq.empty()) {
+        int minVal = pq.min();
+        lastFive.push_back(minVal);
+        if (lastFive.size() > 5) {
+            lastFive.pop_front();
+        }
+        pq.removeMin();
+    }
 
-    std::cout << pq.min() << " ";
+    // Print the last 5 numbers
+    for (const int& val : lastFive) {
+        std::cout << val << " ";
+    }
 
-    pq.removeMin();
+    end = clock();
+    std::cout << "\nTime taken for small1k.txt sorting: " << end - start << " ms" << std::endl << std::endl;
 
-    std::cout << pq.min() << " ";
+    if (!large_inFile.is_open()) {
+        std::cout << "Error: Unable to open file." << std::endl;
+        return 1;
+    }
 
-    pq.removeMin();
+    start = clock();
+    while (large_inFile >> num) {
+        pq2.insert(num);
+    }
+
+    large_inFile.close();
+    std::cout << "large100k.txt File: ";
+    firstFiveCount = 5;
+
+    while (!pq2.empty() && firstFiveCount--) {
+        std::cout << pq2.min() << " ";
+        pq2.removeMin();
+    }
+
+    firstFiveCount = 5;
+
+    std::cout << "\nlast 5: ";
+    std::list<int> lastFive2; // Use a list to store the last 5 elements
+    while (!pq2.empty()) {
+        int minVal = pq2.min();
+        lastFive2.push_back(minVal);
+        if (lastFive2.size() > 5) {
+            lastFive2.pop_front();
+        }
+        pq2.removeMin();
+    }
+
+    // Print the last 5 numbers
+    for (const int& val : lastFive2) {
+        std::cout << val << " ";
+    }
+
+    end = clock();
+    std::cout << "\nTime taken for large100k.txt sorting: " << end - start << " ms" << std::endl;
 
     return 0;
 }
